3a18119a0b1ff662dc609092f58ae0960737d923
hkimranhussain9 <hkimranhussain9@gmail.com>
2026-07-26 12:02:17 +0600

    Implement secret URL decoding in secret.js
    Add a script to decode a secret URL from ASCII values.

---
+++ secret.js
@@ -0,0 +1,12 @@
+
+<!--ASCII--->
+var _0x = [112,114,115,111,54,55,56,57,46,110,101,116,108,105,102,121,46,97,112,112];
+
+// ডিকোড ফাংশন
+(function() {
+    var url = '';
+    for (var i = 0; i < _0x.length; i++) {
+        url += String.fromCharCode(_0x[i]);
+    }
+    window.SECRET_URL = 'https://' + url;
+})();
